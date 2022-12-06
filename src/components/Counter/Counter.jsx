919737a27e1318aba2dee06928d3a942284f268f
GeorgeSwift16 <George.Swift@nology.io>
2022-12-06 18:22:01 +0000

    initial styling

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -5,12 +5,14 @@ const Counter = () => {
     <div className="employee__counter-container">
       <h3 className="employee__counter-header">Counter</h3>
       <output className="employee__counter-output">0</output>
-      <button className="employee__counter-input-button button button-minus">
-        -
-      </button>
-      <button className="employee__counter-input-button button button-plus">
-        +
-      </button>
+      <div className="employee__counter-button-container">
+        <button className="employee__counter-button button button-minus">
+          -
+        </button>
+        <button className="employee__counter-button button button-plus">
+          +
+        </button>
+      </div>
     </div>
   );
 };
