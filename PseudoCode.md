b3f162a9e0e24ba68b661c1ee01734ba093c9577
GeorgeSwift16 <George.Swift@nology.io>
2022-12-06 15:24:06 +0000

    created hardcoded layout

---
+++ PseudoCode.md
@@ -0,0 +1,19 @@
+# Ticket tracker
+
+## inputs
+
+1. employee data - arry of objects, each object - id: number, name: string, role: string
+
+## outputs
+
+1. ticket tracker for each employee
+2. grid of all ticket trackers
+3. ability to increment the tracker - state
+4.
+
+## methodology
+
+1. Components for container > employee > counter
+2. need to take in the employee array and map it to the employee component
+3. need to setup a grid component and map the employee component to the squares.
+4. need to setup a counter component (two buttons) to enable incrementing of the count
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import "./App.scss";
-import team from "./data/team";
+// import team from "./data/team";
 
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <>
+      <h1>Ticket Tracker</h1>
+      <section className="grid-container">
+        <div className="employee">
+          <h2 className="employee__header">Name</h2>
+          <h3 className="employee__subheader">Role</h3>
+          <div className="employee__counter-container">
+            <h3 className="employee__counter-header">Counter</h3>
+            <output className="employee__counter-output">0</output>
+            <button className="employee__counter-input-button button button-minus">
+              -
+            </button>
+            <button className="employee__counter-input-button button button-plus">
+              +
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default App;
