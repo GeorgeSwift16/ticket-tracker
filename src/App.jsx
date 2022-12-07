0666e62b68d765ebcc19f30e60ff80c7d6772dba
GeorgeSwift16 <George.Swift@nology.io>
2022-12-07 17:30:28 +0000

    got some level of search input return, changing branch to try and fix multiple char issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,52 @@
 import "./App.scss";
 import team from "./data/team";
 import Employee from "./components/Employee/Employee";
+import { useState } from "react";
 
 const App = () => {
   const employeeArr = team.map(({ id, name, role }) => (
     <Employee key={`${id}a`} id={id} name={name} role={role} />
   ));
 
+  const [searchInput, setSearchInput] = useState("");
+  const getNewSearchInput = (event) => {
+    let currentSearchInput = "";
+    currentSearchInput += event.target.value;
+    setSearchInput(0);
+    setSearchInput(currentSearchInput);
+    console.log(searchInput);
+    updateDisplayedTickets(searchInput);
+  };
+  const updateDisplayedTickets = (inputstring) => {
+    console.log(searchInput);
+    let searchFilteredEmployeeArr = [];
+    searchFilteredEmployeeArr = team
+      .filter((element) => element.name.includes(inputstring))
+      .map(({ id, name, role }) => (
+        <Employee key={`${id}a`} id={id} name={name} role={role} />
+      ));
+    return searchFilteredEmployeeArr;
+  };
+
   return (
     <>
       <h1>Ticket Tracker</h1>
-      <section className="grid-container">{employeeArr}</section>
+      <label htmlFor="search-by-name">
+        Search by name
+        <input
+          onChange={getNewSearchInput}
+          name="search-by-name"
+          type="text"
+          value={searchInput}
+        />
+      </label>
+      {searchInput ? (
+        <section className="grid-container">
+          {updateDisplayedTickets(searchInput)}
+        </section>
+      ) : (
+        <section className="grid-container">{employeeArr}</section>
+      )}
     </>
   );
 };
